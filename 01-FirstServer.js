0a0bd0822125b72db7b02f99bfe06935fff542c1
Todd Croak-Falen <Todd Croak-Falen>
2016-09-19 19:12:28 -0700

    Upload js file.

---
+++ 01-FirstServer.js
@@ -0,0 +1,26 @@
+//Require/import the HTTP module
+var http = require('http');
+
+//Lets define a port we want to listen to
+var PORT1 = 7000;
+var PORT2 = 7500;
+
+//We need a function which handles requests and send response
+function handleRequest(request, response){
+    response.end('It Works!! Path Hit: ' + request.url);
+}
+
+//Create a server
+var server1 = http.createServer(handleRequest);
+var server2 = http.createServer(handleRequest);
+
+//Lets start our server
+server1.listen(PORT1, function(){
+    //Callback triggered when server is successfully listening. Hurray!
+    console.log("You look good today!");
+});
+
+server2.listen(PORT2, function(){
+    //Callback triggered when server is successfully listening. Hurray!
+    console.log("You look like shit.");
+});
